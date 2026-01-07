f1a726880ab43dd03faf860a87c0a9d066348324
Luiz <luizzlh2004@gmail.com>
2026-01-06 21:29:40 -0300

    Adicionado records para receber dados da API, adicionado ENUM para especialidade e por nos records, adicionado Controller para medicos e adicionado as dependencias.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
     }
 
 }
